c9d87db76a21248e3858f236fe6ae3f0ea1e60b9
adriavieira314 <adriavieira314@gmail.com>
2022-01-04 19:27:28 -0400

    novos campos da tabela

--- src/app/pages/etiquetas-gerencial/etiquetas-gerencial.component.ts
+++ src/app/pages/etiquetas-gerencial/etiquetas-gerencial.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class EtiquetasGerencialComponent implements OnInit {
   filter: string = '';
   dadosClientes = [
-    {cliente: 'Honda', totalCarro: '100', carrosCliente:'80', retornoCarro: '20', dataUltimaSaida: '2021-12-20', dataUltimoRetorno: '2021-12-20' },
-    {cliente: 'Samsung', totalCarro: '80', carrosCliente:'50', retornoCarro: '30', dataUltimaSaida: '2021-12-17', dataUltimoRetorno: '2021-12-17' },
-    {cliente: 'Cliente1', totalCarro: '50', carrosCliente:'25', retornoCarro: '25', dataUltimaSaida: '2021-12-17', dataUltimoRetorno: '2021-12-17' },
-    {cliente: 'Cliente2', totalCarro: '50', carrosCliente:'30', retornoCarro: '20', dataUltimaSaida: '2021-12-17', dataUltimoRetorno: '2021-12-17' },
+    {etiqueta: '1542', cliente: 'Honda', totalAtivo: '100', dataUltimaSaida: '2021-12-20', dataUltimoRetorno: '2021-12-20', status: 'Ativo' },
+    {etiqueta: '0023', cliente: 'Samsung', totalAtivo: '80', dataUltimaSaida: '2021-12-17', dataUltimoRetorno: '2021-12-17', status: 'Manutenção' },
+    {etiqueta: '7896', cliente: 'PAM', totalAtivo: '50', dataUltimaSaida: '2021-12-17', dataUltimoRetorno: '2021-12-17', status: 'Desativado' },
+    {etiqueta: '5698', cliente: 'PAM', totalAtivo: '50', dataUltimaSaida: '2021-12-17', dataUltimoRetorno: '2021-12-17', status: 'PAM' },
   ];
 
   constructor() { }
