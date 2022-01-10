c803793d09c54307c273f16065ea79f39ef04208
adriavieira314 <adriavieira314@gmail.com>
2022-01-09 22:23:04 -0400

    ajustando filter

--- src/app/components/pipes/filter.pipe.ts
+++ src/app/components/pipes/filter.pipe.ts
@@ -7,10 +7,10 @@ export class FilterPipe implements PipeTransform {
 
    transform(value: any, searchValue: string): any {
       if (!searchValue) return value;
-      return value.filter((v: any) => v.cliente.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 ||
-      v.etiqueta.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 ||
-      v.dataUltimaSaida.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 ||
-      v.dataUltimoRetorno.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
+      return value.filter((v: any) => v.code.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 ||
+      v.cartIssues[0].client.name.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 ||
+      v.cartIssues[0].readAt.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 ||
+      v.cartIssues[0].cartReturnal?.readAt.toLowerCase().indexOf(searchValue.toLowerCase()) > -1);
    }
 
 }
