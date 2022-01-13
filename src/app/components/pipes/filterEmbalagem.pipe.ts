197070481a0c1a34d84464c7c22745553bf42356
adriavieira314 <adriavieira314@gmail.com>
2022-01-12 22:43:19 -0400

    filtros ajustados

--- src/app/components/pipes/filterEmbalagem.pipe.ts
+++ src/app/components/pipes/filterEmbalagem.pipe.ts
@@ -7,6 +7,6 @@ export class FilterEmbalagemPipe implements PipeTransform {
 
    transform(value: any, searchValue: string): any {
       if (!searchValue) return value;
-      return value.filter((v: any) =>  v.carros.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
+      return value.filter((v: any) =>  v.name.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
    }
 }
