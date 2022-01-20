26f56abc74399730945018cf746b1c513387f2f6
adriavieira314 <adriavieira314@gmail.com>
2022-01-20 19:28:47 -0400

    Listar somente carrinhos com status OUT

--- src/app/pages/etiquetas/etiquetas-gerencial/etiquetas-gerencial.component.ts
+++ src/app/pages/etiquetas/etiquetas-gerencial/etiquetas-gerencial.component.ts
@@ -37,7 +37,10 @@ export class EtiquetasGerencialComponent implements OnInit {
     this.etiquetasService.getEtiquetas().subscribe((etiquetas: any) => {
       // cartIssues pode vir vazio, aqui preencho somente quanto tiver objeto em cartIssues
       etiquetas.forEach((element: any) => {
-        if (element.cartIssues.length >= 1) {
+        console.log(element.status);
+        
+        // if (element.cartIssues.length >= 1) {
+        if (element.status === 'OUT') {
           this.dadosClientes.push(element);
           // console.log(this.dadosClientes);
           
