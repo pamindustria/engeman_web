b09a6a846676659539656d0f06d102956e213647
pamindustria <levy.saraiva@pamplasticos.ind.br>
2022-02-07 15:30:28 -0300

    error XMLHttpRequest endpoint

--- src/app/pages/engeman-gerencial/backend/services/engeman-gerencial.service.ts
+++ src/app/pages/engeman-gerencial/backend/services/engeman-gerencial.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-const API_URL = 'http://10.30.6.96:8000/api/'
+const API_URL = 'http://10.30.6.96:8000/api'
 
 @Injectable({ providedIn: 'root' })
 export class EngemanGerencialService {
@@ -9,6 +9,6 @@ export class EngemanGerencialService {
   constructor(private http: HttpClient) { }
 
   getEngemanList() {
-    return this.http.get(`${API_URL}/getOS`);
+    return this.http.get(`${API_URL}/getOS/`);
   }
 }
