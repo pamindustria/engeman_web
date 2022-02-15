6709817601daff4808abd877934f5b791ba75e63
pamindustria <levy.saraiva@pamplasticos.ind.br>
2022-02-14 22:25:10 -0300

    codigo para identificação das urls

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 const port = 3000;
 
 const app = express();
@@ -14,6 +15,11 @@ app.use(bodyParser.json());
 
 app.use(express.static('./dist/tela-gerencial/'));
 
-app.get('/', (request, response) => response.sendFile('./dist/tela-gerencial/index.html'));
+// app.get('*', (request, response) => response.sendFile('./dist/tela-gerencial/index.html'));
+
+//angular routing funcionando a partir daqui
+app.get('*',(req,res) =>{
+    res.sendFile(path.join(__dirname,'dist/tela-gerencial/index.html'));
+});
 
 app.listen(port, '0.0.0.0', () => console.info(`Servidor rodando na porta: ${port}`));
