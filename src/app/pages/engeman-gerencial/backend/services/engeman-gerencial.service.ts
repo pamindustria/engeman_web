b46a58e28cf552eff3c017796297398953733eb4
adriavieira314 <adriavieira731@gmail.com>
2022-03-08 16:27:04 -0400

    retornando

--- src/app/pages/engeman-gerencial/backend/services/engeman-gerencial.service.ts
+++ src/app/pages/engeman-gerencial/backend/services/engeman-gerencial.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-const API_URL = 'https://10.30.2.253:8000/api'
+const API_URL = 'http://10.30.2.253:8000/api'
 
 @Injectable({ providedIn: 'root' })
 export class EngemanGerencialService {
